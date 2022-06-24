62249df2380515fe28aad332853189aa5050da96
beratios <beratgultekin34@gmail.com>
2022-06-24 18:38:09 +0300

    Initial Commit

---
+++ catchSpongeBob/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  catchSpongeBob
+//
+//  Created by Berat Gültekin on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ catchSpongeBob/catchSpongeBobApp.swift
@@ -0,0 +1,17 @@
+//
+//  catchSpongeBobApp.swift
+//  catchSpongeBob
+//
+//  Created by Berat Gültekin on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct catchSpongeBobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
