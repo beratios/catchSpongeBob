a665ae71047d40cf7ae8af897b5190dd53d0f256
beratios <beratgultekin34@gmail.com>
2022-07-18 14:16:51 +0300

    first commit

---
+++ catchSpongeBob/welcomePage.swift
@@ -0,0 +1,20 @@
+//
+//  welcomePage.swift
+//  catchSpongeBob
+//
+//  Created by Berat Gültekin on 29.06.2022.
+//
+
+import SwiftUI
+
+struct welcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct welcomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        welcomePage()
+    }
+}
